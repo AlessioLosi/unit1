7aec4f7eb263973f1fb7bc56e8a2babf45f57695
AlessioLosi <alessiolosi93@icloud.com>
2024-06-20 23:30:47 +0200

    esercizi giorno 2 JavaScript

--- unità1/settimana2/giorno2/assets/js/D2.js
+++ unità1/settimana2/giorno2/assets/js/D2.js
@@ -5,13 +5,13 @@
 
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let myNumber=10;
-let myNumber2=5;
+let myNumber = 10;
+let myNumber2 = 5;
 
-if( myNumber> myNumber2){
-  console.log('il numero più grande è '+ myNumber)
-} else{
-console.log( 'il numero più grande è' + myNumber2)
+if (myNumber > myNumber2) {
+  console.log('il numero più grande è ' + myNumber)
+} else {
+  console.log('il numero più grande è' + myNumber2)
 }
 
 /* ESERCIZIO 2
@@ -20,20 +20,20 @@ console.log( 'il numero più grande è' + myNumber2)
 
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-if(myNumber!== 5)
-  {console.log ('not equal')}
-else{
-  console.log('equal')}
+if (myNumber !== 5) { console.log('not equal') }
+else {
+  console.log('equal')
+}
 
 /* ESERCIZIO 3
   Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let myNumber3= 25
-if((myNumber3 % 5)===0){
+let myNumber3 = 25
+if ((myNumber3 % 5) === 0) {
   console.log('è divisibile per 0')
-}else{
+} else {
   console.log('non è divisibile per 0')
 }
 
@@ -42,8 +42,17 @@ if((myNumber3 % 5)===0){
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let myNumber4= 5
-let myNumber5= 2
+let myNumber4 = 5
+let myNumber5 = 13
+if (myNumber4 === 8 || myNumber5 === 8) {
+  console.log('uno dei due valori è uguale a 8')
+} else if ((myNumber4 + myNumber5) === 8) {
+  console.log('la loro somma è uguale 8')
+} else if (myNumber4 - myNumber5 === 8 || myNumber5 - myNumber4 === 8) {
+  console.log('la loro sottrazione è uguale a 8')
+} else {
+  console.log('nessuno dei due valori è uguale a 8, nè la loro addizione o sottrazione')
+}
 
 /* ESERCIZIO 5
   Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
@@ -52,6 +61,13 @@ let myNumber5= 2
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let totalShoppingCart = 30
+if (totalShoppingCart > 50) {
+  console.log(totalShoppingCart);
+} else {
+  console.log(totalShoppingCart + 10);
+
+}
 
 /* ESERCIZIO 6
   Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
@@ -59,6 +75,14 @@ let myNumber5= 2
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let totalShoppingCart2 = 70;
+let totalDiscount = totalShoppingCart2 * 0.8;
+if (totalDiscount > 50) {
+  console.log(totalDiscount);
+} else {
+  console.log(totalDiscount + 10);
+}
+
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -67,18 +91,48 @@ let myNumber5= 2
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let val1=30;
+let val2= 15;
+let val3= 40;
+
+if (val1>val2 && val2>val3){
+  console.log( val1, val2, val3);
+} else if(val1>val3 && val3>val2){
+  console.log( val1, val3, val2);
+}else if(val2>val1 && val1>val3){
+  console.log( val2, val1, val3);
+}else if(val2>val3 && val3>val1){
+  console.log( val2, val3, val1);
+}else if(val3>val1 && val1>val2){
+  console.log( val3, val1, val2);
+}else  {
+  console.log( val3, val2, val1)
+}
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let myNumber6= 18;
+if(typeof myNumber6=== "number"){
+  console.log ('il valore è un numero');
+} else{
+  console.log('il valore non è un numero');
+}
+
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+val4=4
+if((val4%2)===0){
+  console.log('il numero è pari')
+} else{
+  console.log('il numero è dispari')
+}
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -93,6 +147,15 @@ let myNumber5= 2
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let val =3
+  if (val < 10 && val >= 5) {
+      console.log("Meno di 10");
+    } else if (val < 5) {
+      console.log("Meno di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }
+
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -105,27 +168,49 @@ const me = {
 }
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+me.city= 'Toronto'
+console.log(me);
+
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+delete me.lastName
+console.log(me);
+
 
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+me.skills.length=me.skills.length-1
+
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const list=[];
+list[0]=1;
+list[1]=2;
+list[2]=3;
+list[3]=4;
+list[4]=5;
+list[5]=6;
+list[6]=7;
+list[7]=8;
+list[8]=9;
+list[9]=10;
 
+console.log(list);
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+list[9]=100;
+console.log(list)
--- unità1/settimana2/giorno3/assets/js/D3.js
+++ unità1/settimana2/giorno3/assets/js/D3.js
@@ -104,16 +104,24 @@ const starWarsCharacters = [
 /* ESERCIZIO 1
   Crea una variabile chiamata "charactersNames" e assegnale un array vuoto
 */
+const charactersNames=[]
 
 /* ESERCIZIO 2
   Utilizzando un ciclo for, cicla l'array "starWarsCharacters".
   Dovrai accedere alla proprietà "name" di ogni oggetto in esso contenuto, e inserirla nell'array "charactersNames" creato precedentemente.
   Come risultato dovresti ottenere qualcosa di simile: ["Luke Skywalker", "C-3PO", "R2-D2", etc..]
 */
+for(let i=0 ; i< starWarsCharacters.length; i++)
+   charactersNames = starWarsCharacters[i]
+
+charactersNames.push(characters.names)
+console.log( charactersNames)
+  
 
 /* ESERCIZIO 3
   Seguendo i passaggi precedenti crea un nuovo array chiamato "femaleCharacters" e inserisci al suo interno tutti gli oggetti femminili.
 */
+
 
 /* ESERCIZIO 4
   Crea un oggetto "eyeColor" che abbia le seguenti proprietà: blue, yellow, brown, red, blue-gray.
